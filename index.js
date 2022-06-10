d66405d147f60b1680e4e36d76deca210fa7d055
chrispus1000 <minrevac@gmail.com>
2022-06-10 16:36:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+const record = [
+    {year: "2022", result: "L"},
+    {year: "2021", result: "W"},
+    {year: "2020", result: "N/A"}
+  ] 
+
+function superbowlWin(array) {
+    let result = array.find(element => element.result === "W")
+    return !!result ? result.year: undefined
+}
